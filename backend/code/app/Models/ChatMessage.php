2f04f3e889195aad57349d0ed5fb45de7f3e094e
ezznerf <yakuta04@mail.ru>
2025-02-24 22:48:14 +0300

    make models and migrations for chat

---
+++ backend/code/app/Models/ChatMessage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $chat_id
+ * @property int $user_id
+ * @property string $content
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class ChatMessage extends Model
+{
+    //
+}
